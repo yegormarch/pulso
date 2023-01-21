162090294424e5e761c662e45da7f2fdf4303e29
Yegor <marchenko.yegor.gmail.com>
2023-01-21 04:05:26 -0300

    feat:plane commit

--- src/JS/script.js
+++ src/JS/script.js
@@ -1,18 +1,35 @@
-$(document).ready(function(){
-  $('.carousel__inner').slick({
-    infinite: true,
-    speed: 300,
-    slidesToShow: 1,
-    adaptiveHeight: true,
-    prevArrow: '<button type="button" class="slick-prev"><img src = "icons/chevron_left.png"></button>',
-    nextArrow: '<button type="button" class="slick-next"><img src = "icons/chevron_right.png"></button>',
-    responsive: [
-      {
-        breakpoint: 768,
-        settings: {
-          arrows: false,
-          dots: true
-        }
-      }]
-  });
+// $(document).ready(function(){
+//   $('.carousel__inner').slick({
+//     infinite: true,
+//     speed: 300,
+//     slidesToShow: 1,
+//     adaptiveHeight: true,
+//     prevArrow: '<button type="button" class="slick-prev"><img src = "icons/chevron_left.png"></button>',
+//     nextArrow: '<button type="button" class="slick-next"><img src = "icons/chevron_right.png"></button>',
+//     responsive: [
+//       {
+//         breakpoint: 768,
+//         settings: {
+//           arrows: false,
+//           dots: true
+//         }
+//       }]
+//   });
+// });
+
+const slider = tns ({
+  container: '.carousel__inner',
+  items: 1,
+  slideBy: 'page',
+  autoplay: false, 
+  controls: false,
+  nav: false
 });
+
+document.querySelector('.prev').onclick = function () {
+  slider.goTo('prev');
+};
+
+document.querySelector('.next').onclick = function () {
+  slider.goTo('next');
+};
